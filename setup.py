4f5baee08605dbc443a3395b1174ba8722551cce
Frederik Bolding <frederik.bolding@gmail.com>
2019-07-09 22:32:01 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bt_proximity',
-      version='0.1',
+      version='0.2',
       description="Allows for querying of the RSSI values for nearby devices using pybluez. Ported to Python 3, based on https://github.com/ewenchou/bluetooth-proximity",
       url='https://github.com/FrederikBolding/bluetooth-proximity',
       author='Frederik Bolding',
